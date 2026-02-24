6e0744c5245c715c5fd0fe6960738cac00035c9f
FENGFANCHEN-012 <Chensail@outlook.com>
2026-02-24 20:26:41 +0800

    bug fix

--- ReactNative/backend-laravel/routes/api.php
+++ ReactNative/backend-laravel/routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/user/signup',[UserController::class ,'store']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/user/signup',[UserController::class ,'store']);
 
 Route::get('/test', function () {
     return response()->json([
