003d9a79b70f4cbe657bacf203912e98c47110ac
FENGFANCHEN-012 <Chensail@outlook.com>
2026-03-03 17:51:20 +0800

    bug fix

--- ReactNative/backend-laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ ReactNative/backend-laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-          
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('username')->unique()->nullable();
+            $table->string('sex')->nullable();
+            $table->integer('age')->nullable();
+            $table->string('ProfilePicture')->nullable();
+            $table->string('role')->default('user');
+            $table->string('phone_number')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->boolean('first_login')->default(true);
+            $table->rememberToken();
+            $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
