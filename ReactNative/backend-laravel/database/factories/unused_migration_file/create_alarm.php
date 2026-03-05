49b0eaec1aff5d624a7957d4dcedb888a3ba6204
FENGFANCHEN-012 <Chensail@outlook.com>
2026-03-05 09:16:07 +0800

    bug fix

--- ReactNative/backend-laravel/database/factories/unused_migration_file/create_alarm.php
+++ ReactNative/backend-laravel/database/factories/unused_migration_file/create_alarm.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -42,4 +42,4 @@ return new class extends Migration
     }
   
 };
-  */
+  
--- ReactNative/backend-laravel/database/factories/unused_migration_file/create_goal.php
+++ ReactNative/backend-laravel/database/factories/unused_migration_file/create_goal.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -34,4 +34,4 @@ return new class extends Migration
       
     }
 };
-    */
+    
